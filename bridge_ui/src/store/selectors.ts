252812e9ea6e1ee6ac7e954432c152ab3f4e08ed
Evan Gray <battledingo@gmail.com>
2021-10-28 11:54:49 -0400

    bridge_ui: safely set target chain in recovery
    Change-Id: I7d6e03b56b61129d72ef641059871ff362a3f0f4

--- bridge_ui/src/store/selectors.ts
+++ bridge_ui/src/store/selectors.ts
@@ -121,10 +121,7 @@ export const selectNFTTargetError = (state: RootState) => {
   if (state.nft.sourceChain === state.nft.targetChain) {
     return "Select a different target and source";
   }
-  if (
-    state.nft.targetChain === CHAIN_ID_SOLANA &&
-    !selectNFTTargetAsset(state)
-  ) {
+  if (state.nft.targetChain === CHAIN_ID_SOLANA && !state.nft.targetAsset) {
     // target asset is only required for solana
     // in the cases of new transfers, target asset will not exist and be created on redeem
     // Solana requires the derived address to derive the associated token account which is the target on the vaa
@@ -261,7 +258,7 @@ export const selectTransferTargetError = (state: RootState) => {
   if (state.transfer.sourceChain === state.transfer.targetChain) {
     return "Select a different target and source";
   }
-  if (!selectTransferTargetAsset(state)) {
+  if (!state.transfer.targetAsset) {
     return UNREGISTERED_ERROR_MESSAGE;
   }
   if (
