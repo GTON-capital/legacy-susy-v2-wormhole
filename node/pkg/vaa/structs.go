99e3ccd444f28433648c285889205540f967bdc3
Leo <leo@certus.one>
2021-10-29 17:17:11 +0200

    node/cmd/guardiand: governance template improvements
    The template commands now support generating full governance messages
    with all required fields. Outputs to stdout instead of a file.
    
    Change-Id: I3837107c3075363a54d31f9dfb4d6dc07c79daa5

--- node/pkg/vaa/structs.go
+++ node/pkg/vaa/structs.go
@@ -7,6 +7,7 @@ import (
 	"encoding/hex"
 	"fmt"
 	"io"
+	"strings"
 	"time"
 
 	"github.com/ethereum/go-ethereum/common"
@@ -87,6 +88,25 @@ func (c ChainID) String() string {
 		return name
 	}
 	return fmt.Sprintf("unknown chain ID: %d", c)
+}
+
+func ChainIDFromString(s string) (ChainID, error) {
+	s = strings.ToLower(s)
+
+	switch s {
+	case "solana":
+		return ChainIDSolana, nil
+	case "ethereum":
+		return ChainIDEthereum, nil
+	case "terra":
+		return ChainIDTerra, nil
+	case "bsc":
+		return ChainIDBSC, nil
+	case "polygon":
+		return ChainIDPolygon, nil
+	default:
+		return ChainIDUnset, fmt.Errorf("unknown chain ID: %s", s)
+	}
 }
 
 const (
