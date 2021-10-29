36319cb83af848ecca11d6f76da49fc41858e10d
Reisen <dparyente@jumptrading.com>
2021-10-29 10:24:05 +0000

    terra/token_bridge: migration prep
    Change-Id: I3bc054265e6296ce2fa4191c0395ca8531b0f4a8

--- terra/contracts/token-bridge/src/contract.rs
+++ terra/contracts/token-bridge/src/contract.rs
@@ -131,7 +131,7 @@ pub fn migrate(deps: DepsMut, _env: Env, _msg: MigrateMsg) -> StdResult<Response
                 .api
                 .addr_humanize(&contract_address.into())?
                 .to_string(),
-            new_code_id: 3,
+            new_code_id: 767,
             msg: to_binary(&MigrateMsg {})?,
         }));
     }
@@ -489,8 +489,8 @@ fn handle_create_asset_meta_native_token(
         token_chain: CHAIN_ID,
         token_address: asset_id.clone(),
         decimals: 6,
-        symbol: extend_string_to_32(&symbol)?,
-        name: extend_string_to_32(&symbol)?,
+        symbol: extend_string_to_32(&symbol),
+        name: extend_string_to_32(&symbol),
     };
     let token_bridge_message = TokenBridgeMessage {
         action: Action::ATTEST_META,
