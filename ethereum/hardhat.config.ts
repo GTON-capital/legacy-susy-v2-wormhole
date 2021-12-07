0e112eb48855d5e4690ada3944d0c15456bf745a
Shamil Khalilov <shamfama@gmail.com>
2021-12-07 19:00:54 +0300

    add hardhat support

---
+++ ethereum/hardhat.config.ts
@@ -0,0 +1,67 @@
+import "@typechain/hardhat";
+import "@nomiclabs/hardhat-ethers";
+import "@nomiclabs/hardhat-waffle";
+import "hardhat-abi-exporter";
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+export default {
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545",
+    },
+    avax: {
+      url: "https://api.avax.network/ext/bc/C/rpc",
+      accounts: [],
+    },
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.4",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+      {
+        version: "0.6.6",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+      {
+        version: "0.6.12",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+      {
+        version: "0.5.16",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
+  },
+  abiExporter: {
+    clear: true,
+    flat: true,
+    spacing: 2,
+  },
+  mocha: {
+    timeout: "100000000000000",
+  },
+};
